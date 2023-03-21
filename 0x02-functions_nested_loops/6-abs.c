66ecc53e2df6422fdea92e8bf9a8cfdc88321c27
ebubechukwuugochukwu1 <ebubechukwu.ugochukwu1@gmail.com>
2023-03-21 12:26:11 -0700

    update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs_ Computers the absolute value of an integer.
+ * _abs - Computers the absolute value of an integer.
  * @c: The number to be computed.
  * Return: Absolute value of number or zero
  */
