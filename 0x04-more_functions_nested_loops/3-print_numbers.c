f20491c6599816b76ad861320fe42a4fd3f61b3b
ebubechukwuugochukwu1 <ebubechukwu.ugochukwu1@gmail.com>
2023-03-23 11:21:52 -0700

    update from question no 3 to 6. The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - printsfro  0-9
- * Return: Always 0
+ * print_numbers - print the number since 0 up to 9
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
 
 {
-	int a = 0;
+	int x;
 
-	while (a < 10)
+	for (x = 0; x <= 9; x++)
 	{
-	_putchar(a++ + '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
