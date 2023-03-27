5f03750d5efb737dc2b4d034afdc9c8edb1ed488
ebubechukwuugochukwu1 <ebubechukwu.ugochukwu1@gmail.com>
2023-03-27 11:49:21 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -33,10 +33,22 @@ int main(void)
 		if ((sum - 2772) % 2 != 0)
 		diff_half1++;
 
+		{
+			if (password[index] >= (33 + diff_half1))
+
+			{
+				password[index] -= diff_half1;
+
+				break;
+
+			}
+		}
+
 		for (index = 0, password[index]; index++)
+
 		{
 
-			if (password[index] >= (33 + diff_half1))
+			if (password[index] >= (33 + diff_half2))
 
 			{
 				password[index] -= diff_half2;
